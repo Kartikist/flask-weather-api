50e6e36826bfab7ae9ccc0a60021dca8e4d59159
Kartik Neti <kartikprajak@gmail.com>
2023-10-24 21:05:45 +0530

    Added endpoints for station wise and station+year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import pandas as pd
+import matplotlib
 
 
 app = Flask(__name__)
@@ -33,6 +34,21 @@ def call_api(station, date):
             "date": date,
             "temperature": temperature}
 
+@app.route("/api/v1/<station>/")
+def station_only(station):
+    filename= "data_small\TG_STAID" + str(station).zfill(6) +".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    dish = df.to_dict(orient="records")
+    # plot = df.loc[df['   TG'] != -9999]['   TG'].hist()
+    return dish
 
+@app.route("/api/v1/annual/<station>/<year>/")
+def annual(year,station):
+    filename= "data_small\TG_STAID" + str(station).zfill(6) +".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE']=df['    DATE'].astype(str)
+    yearly_result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient="records")
+    return yearly_result
+    
 if __name__ == '__main__':
     app.run(debug=True)
